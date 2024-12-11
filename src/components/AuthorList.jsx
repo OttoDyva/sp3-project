3cd7aa6705f7aa18af885fea66d537dee40df790
BekhanM <therealbekhan@gmail.com>
2024-12-11 13:42:49 +0100

    v0.6.1.2

--- src/components/AuthorList.jsx
+++ src/components/AuthorList.jsx
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from "react";
 import facade from "../util/apiFacade";
-import '../css/Author.css'
+import "../css/Author.css";
 
 const AuthorList = ({ onSelectAuthor }) => {
   const [authors, setAuthors] = useState([]);
 
   useEffect(() => {
     const fetchAuthors = async () => {
-      const authors = await facade.fetchData("/api/authors");
-      setAuthors(authors);
+      try {
+        const authors = await facade.fetchData("/api/authors");
+        setAuthors(authors);
+      } catch (error) {
+        console.error("Error fetching authors:", error);
+      }
     };
     fetchAuthors();
   }, []);
+
+  const deleteAuthorById = async (authorId) => {
+    try {
+      await facade.deleteData(`/api/authors/${authorId}`);
+      setAuthors(authors.filter((author) => author.id !== authorId));
+    } catch (error) {
+      console.error("Error deleting author:", error);
+    }
+  };
+  
 
   return (
     <div className="author-list-container">
       <h2 className="author-list-header">Authors</h2>
       <div className="author-list-grid">
         {authors.map((author) => (
-          <div
-            key={author.id}
-            onClick={() => onSelectAuthor(author.id)}
-            className="author-card"
-          >
+          <div key={author.id} className="author-card">
             <h3 className="author-name">{author.name}</h3>
             <p className="author-description">{author.description}</p>
+            <button onClick={() => deleteAuthorById(author.id)}>Delete</button>
           </div>
         ))}
       </div>
