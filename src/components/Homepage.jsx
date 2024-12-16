add2c7426d44f9a1e6ea3e5e2faace39e356ccfc
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2024-12-16 14:06:39 +0100

    Frontpage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import "../css/Home.css";
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Welcome to the homepage!</h1>
+    <div className="home">
+      <h1>Welcome to Bars Quotes</h1>
+      <h2>Where Words Drop Harder Than the Beat</h2>
+      <p>
+        Lost in words? Obsessed with quotes that hit harder than your morning coffee?
+        You’ve found your new home. At Bars Quotes, we celebrate everything from
+        poetic gems to motivational fire, hilarious one-liners to totally
+        ridiculous quotes that make you wonder what just happened. 
+      </p>
+      <br />
     </div>
   );
 };
